7b5a1734579c0698806ff80daa831d006449cbeb
Kira9112 <kira91_911@mail.ru>
2021-07-14 01:06:37 +0300

    Initial commit

---
+++ src/main/java/ru/netology/StatisticsService.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+public class StatisticsService<incomes> {
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max_index= 0;
+        long current_max = incomes[1];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min_index= 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+
+    }
+}
