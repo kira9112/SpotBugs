00b99b3ad82307650617ff0af73cc0a72c74411d
Kira9112 <kira91_911@mail.ru>
2021-07-14 16:08:37 +0300

    update pom.xml

--- src/main/java/ru/netology/StatisticsService.java
+++ src/main/java/ru/netology/StatisticsService.java
@@ -16,14 +16,4 @@ public class StatisticsService<incomes> {
         return current_max;
 
     }
-
-    public long findMin(long[] incomes) {
-        long current_min_index= 0;
-        long current_min = incomes[0];
-        for (long income : incomes)
-            if (current_min > income)
-                current_min = income;
-        return current_min;
-
-    }
 }
